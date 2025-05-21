337f9aa8666536d486de52ebc9f7ea90bc5ac244
UnexomWid <uw@exom.dev>
2025-05-21 15:25:07 +0300

    remove: temp code

--- Assets/Scripts/LawManager.cs
+++ Assets/Scripts/LawManager.cs
@@ -31,11 +31,6 @@ public class LawManager : ScriptableObject
     {
         var law = _laws[UnityEngine.Random.Range(0, _laws.Count)];
 
-        // COM: delete this debugging code
-        if (_laws.Count == 3)
-        {
-            law = _laws[2];
-        }
         _laws.Remove(law);
         return law;
     }
