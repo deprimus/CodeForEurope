c2ccf9059410ef867c9d74175a1d8656ee3ef5f7
Andrei Mihaita <mihaitaandrei86@gmail.com>
2026-03-15 21:22:26 +0200

    Add Ending Logic for GameEnd Prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -134,8 +134,8 @@ public class GameManager : MonoBehaviour
         Transition.SweepIn();
 
         // TODO: Re-enable auto-advance after testing EuroChat/Laptop UI
-        // Tale.Wait(1f);
-        // Tale.Exec(() => OnLibraryEnded());
+        Tale.Wait(1f);
+        Tale.Exec(() => OnLibraryEnded());
     }
 
     public void OnLibraryEnded()
